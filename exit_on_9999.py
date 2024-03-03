09edbd6b88e8766b701346519ac1d218df4fa5df
Makki12 <147034258+Makki12@users.noreply.github.com>
2024-03-03 17:29:30 +0300

    Add files via upload

---
+++ exit_on_9999.py
@@ -0,0 +1,10 @@
+toplam = 0
+y = 0
+print("cikis icin 9999 giriniz")
+while y != 9999 :
+    y = int(input("sayi giriniz: "))
+    toplam = toplam + y
+
+toplam = toplam - 9999
+
+print("toplaminiz:"and toplam)
